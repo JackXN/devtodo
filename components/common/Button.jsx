2f52e3202352d8fd1c16a595632941dba88a9bcf
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-11-12 07:03:00 -0700

    Updated button component

---
+++ components/common/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/button.module.scss';
+
+const Button = () => {
+    return (
+    <>
+<button className={styles.btn}>Create To-Do List</button>
+    </>
+    )
+}
+
+export default Button;
