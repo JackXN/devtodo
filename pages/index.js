01401e16bf1aa1e17ef75f35a6fdf614c4af24c7
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-11-12 06:36:37 -0700

    Updated index sass module

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
 import { Box, Text, Container } from '@chakra-ui/react';
 
 //Utils
@@ -9,6 +9,7 @@ import { Box, Text, Container } from '@chakra-ui/react';
 import Nav from '../components/Nav';
 
 //Styles
+import styles from '../styles/index.module.scss'
 
 export default function Home() {
   return (
